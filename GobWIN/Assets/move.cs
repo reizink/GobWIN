e101b4d24a9f8c7cc502e7e883be96868c87f898
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-11-17 17:02:21 -0500

    Updated Project
    Setup initial area and basic movement

---
+++ GobWIN/Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class move : MonoBehaviour
+{
+
+    NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GobWIN/Assets/playercontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class playercontroller : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
